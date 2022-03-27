b41f0c8cdd5b44a8a6cf2209ec729334d21c43eb
ayushgupta-repo <19ce037@charusat.edu.in>
2022-03-28 03:57:19 +0530

    Extracting necessary information and showing it into the page.

--- my_app/views.py
+++ my_app/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from matplotlib.pyplot import text
 import requests
+
+# Below library handles the whitespaces between the search strings
+from requests.compat import quote_plus
 from bs4 import BeautifulSoup
 
-# Create your views here.
+from . import models
+
+BASE_CRAIGSLIST_URL = 'https://indore.craigslist.org/search/?query={}'
 
 
 def home(request):
@@ -11,8 +17,34 @@ def home(request):
 
 def new_search(request):
     search = request.POST.get('search')
-    print(search)
+    models.Search.objects.create(search=search)
+    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
+    response = requests.get(final_url)
+    data = response.text
+
+    # extracting necessary information from the html code we got
+    soup = BeautifulSoup(data, features='html.parser')
+
+    # below says find all the links that has class result-title
+
+    # Below will provide data in form of list
+    post_listings = soup.find_all('li', {'class': 'result-row'})
+
+    final_postings = []
+
+    for post in post_listings:
+        post_title = post.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+
+        if post.find(class_='result-price'):
+            post_price = post.find(class_='result-price').text
+        else:
+            post_price = 'N/A'
+
+        final_postings.append((post_title, post_url, post_price))
+
     stuff_for_frontend = {
         'search': search,
+        'final_postings': final_postings,
     }
     return render(request, 'my_app/new_search.html', stuff_for_frontend)
